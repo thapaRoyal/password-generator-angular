908df7cd2e6613fb544493bb78604478fe783edb
thapaRoyal <thaparoyal17@gmail.com>
2021-07-26 16:50:14 +0545

    handling text input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  length = 0;
   password = '';
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
