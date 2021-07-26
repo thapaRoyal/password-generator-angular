fc2997b08233cab7809f82345aad82c17e8cfdcb
thapaRoyal <thaparoyal17@gmail.com>
2021-07-26 16:28:57 +0545

    track input state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
 
   onButtonClick() {
+    console.log(this.includeLetters);
     this.password = 'MY PASSWORD !!!';
   }
 }
